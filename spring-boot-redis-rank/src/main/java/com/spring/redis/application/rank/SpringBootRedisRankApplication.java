fff9324a70f388f18ffbbfe85028024fa8901796
fengcheZt <fengchecn@sina.com>
2019-09-27 15:13:51 +0800

    提交rank项目

---
+++ spring-boot-redis-rank/src/main/java/com/spring/redis/application/rank/SpringBootRedisRankApplication.java
@@ -0,0 +1,13 @@
+package com.spring.redis.application.rank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisRankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisRankApplication.class, args);
+    }
+
+}
