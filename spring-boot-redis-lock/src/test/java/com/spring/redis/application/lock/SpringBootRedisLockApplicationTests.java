8586e3d666f43a667ea90321132551d8a440f034
fengcheZt <fengchecn@sina.com>
2019-10-07 17:39:03 +0800

    补全文件

---
+++ spring-boot-redis-lock/src/test/java/com/spring/redis/application/lock/SpringBootRedisLockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.redis.application.lock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRedisLockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
